23ef23ccce2d54bed4869a5c9a35fbd700dd5d22
秃尾巴的猫 <472647301@qq.com>
2020-05-14 18:49:39 +0800

    Update hunts.service.ts

--- src/controllers/hunts/hunts.service.ts
+++ src/controllers/hunts/hunts.service.ts
@@ -13,8 +13,9 @@ export class HuntsService {
   ) {}
 
   async notice(request: Request) {
-    return this.httpService.get(
-      'http://127.0.0.1:8887/get_group_member_list?group_id=296884495',
+    return this.httpService.post(
+      'http://127.0.0.1:8887/get_group_member_list',
+      { group_id: 296884495 },
     );
   }
 }
